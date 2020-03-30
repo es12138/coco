8bd2463c11e61d1e08519763d33231674f8937c1
es <es8716@qq.com>
2020-03-30 16:46:45 +0800

    测试：更新组件初始化

--- Pages/component/component.js
+++ Pages/component/component.js
@@ -6,28 +6,41 @@
 // function hide(){
 //     outLine.Hide();
 // }
-
+const COMPONENT_ID = "873bfcd88a90c67df2fa13d1b4cf2e9e";
 var component = new Component();
 component.Init();
 
 function Component(){
-    this._fatherDom = $("body:first");
-    this._guid = '';
+    this._guid = this.GetGuid();
+    this._id = "component-"+this._guid;
+    this._fatherDom = $("#"+this._id);
 
     this.Init = function(guid){
         this._guid = guid;
-        this._fatherDom.append(this.CreateOutLineDom(guid));
+        this.AddDom();
 
         $('#show-12138').click(this.Show);
-        $('#hide-12138').click(this.Hide);
+        $('#hide-'+this._id).click(this.Hide);
     }.bind(this);
+
+    this.AddDom = function(fatherDom){
+        fatherDom.append(this.CreateFatherDom(this._id));
+        fatherDom.append(this.CreateOutLineDom(this._id));
+        
+        this._fatherDom = $("#"+this._id);
+    };
+
+    this.CreateFatherDom = function(guid){
+        let dom = '<div style="display:none" class="component-12138" id="'+guid+'"></div>';
+        return dom;
+    };
 
     this.CreateOutLineDom = function(guid){
         let dom = "<div>";
         // dom += "<button id='show-"+guid+"' onclick='alert(1);'>alert 1</button>";
         // dom += "<button id='hide-"+guid+"' onclick='alert(2);'>alert 2</button>";
         dom += "<button id='show-12138'>alert 1</button>";
-        dom += "<button id='hide-12138'>alert 2</button>";
+        dom += "<button id='hide-'"+guid+">alert 2</button>";
         dom += "</div>";
         return dom;
     };
@@ -37,5 +50,12 @@ function Component(){
     };
     this.Hide = function(){
         console.log("component: Hide().");
+    };
+
+    this.GetGuid = function () {
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+                return v.toString(16);
+            });
     };
 }
