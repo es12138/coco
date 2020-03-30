e2d8bef3963f51ae1efb723fad75249664f9e69b
es <es8716@qq.com>
2020-03-30 14:40:06 +0800

    测试：更新组件

--- Pages/component/component.js
+++ Pages/component/component.js
@@ -17,15 +17,25 @@ function Component(){
     this.Init = function(guid){
         this._guid = guid;
         this._fatherDom.append(this.CreateOutLineDom(guid));
-    };
+
+        $('#show-12138').click(this.Show());
+        $('#hide-12138').click(this.Hide());
+    }.bind(this);
 
     this.CreateOutLineDom = function(guid){
         let dom = "<div>";
         // dom += "<button id='show-"+guid+"' onclick='alert(1);'>alert 1</button>";
         // dom += "<button id='hide-"+guid+"' onclick='alert(2);'>alert 2</button>";
-        dom += "<button id='show-12138' onclick='alert(1);'>alert 1</button>";
-        dom += "<button id='hide-12138' onclick='alert(2);'>alert 2</button>";
+        dom += "<button id='show-12138'>alert 1</button>";
+        dom += "<button id='hide-12138'>alert 2</button>";
         dom += "</div>";
         return dom;
-    }
+    };
+
+    this.Show = function(){
+        console.log("component: Show().");
+    };
+    this.Hide = function(){
+        console.log("component: Hide().");
+    };
 }
